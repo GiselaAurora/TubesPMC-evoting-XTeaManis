6734f95a0b6446659f7b38bce2a53b9848d43e6a
GiselaAurora <123353805+GiselaAurora@users.noreply.github.com>
2023-05-31 22:20:39 +0700

    Fix and rename dekrip.ino to LED.ino

--- LED.ino
+++ LED.ino
@@ -4,8 +4,8 @@
 #include <stdint.h> // to use uint32_t
 #include <string.h>
 #include <stdlib.h>
-#include <time.h>
-
+#include <time.h> 
+#include <esp32-hal-psram.h>
 int calon1 = 0;
 int calon2 = 0;
 int suara1 = 1;
@@ -19,8 +19,8 @@ char decrypted[33];
 uint32_t key[4] = {0xA56BABCD, 0x56781234, 0xDEF91234, 0x12345678}; // 128-bit key
 // Update these with values suitable for your network.
 
-const char* ssid = "TubagusLT2++";
-const char* password = "kosanxv1b2";
+const char* ssid = "Yang itu";
+const char* password = "passwordnya";
 const char* mqtt_server = "91.121.93.94"; //test.mosqutto.org
 
 WiFiClient espClient;
@@ -40,13 +40,15 @@ void hexToString(char* hex, char* str, int hex_len) {
 }
 
 int decipher(unsigned int pengulangan, char password[17], uint32_t const key[4]) {
-    double time_spent_dechip = 0.0;
-    clock_t begin = clock();
     unsigned int i;
     char buffer[17];
     char output[9]; // hex to string
     uint32_t v[2];
     char decrypted_str[17];
+    // Measure heap usage before decryption
+    uint32_t freeHeapBytesBefore = ESP.getFreeHeap();
+    uint32_t totalHeapBytesBefore = ESP.getHeapSize();
+
     sscanf(password, "%8X%8X", &v[0], &v[1]); // dari password pada akun.txt dipisah jadi dua
     uint32_t v0=v[0], v1=v[1], delta=0x9E3779B9, sum=delta*pengulangan;
     for (i=0; i < pengulangan; i++) {
@@ -56,23 +58,23 @@ int decipher(unsigned int pengulangan, char password[17], uint32_t const key[4])
     }
     v[0]=v0; v[1]=v1;
     sprintf(decrypted_str, "%08X%08X", v[0], v[1]);
-    /*for (i = 0; i < sizeof(decrypted_value)/sizeof(char)/2; i++) {
-        sscanf(&decrypted_str[2*i], "%2X", (unsigned int*)&output[i]);  // Read two hex characters at a time
-    }*/
     hexToString(decrypted_str, output, 16);
+
+    // Measure heap usage after decryption
+    uint32_t freeHeapBytesAfter = ESP.getFreeHeap();
+    uint32_t totalHeapBytesAfter = ESP.getHeapSize();
+
+    // Calculate heap usage
+    uint32_t heapUsageBytes = (totalHeapBytesAfter - freeHeapBytesAfter) - (totalHeapBytesBefore - freeHeapBytesBefore);
+
+    // Print heap usage
+    printf("Decryption Heap Usage: %d bytes\n", heapUsageBytes);
     if (strcmp(output, str1)==0){
       return suara1;
     }
     else if (strcmp(output, str2)==0){
       return suara2;
     }
-    clock_t end = clock(); //untuk mengetahui execution time program
-    time_spent_dechip += (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("Time spent for executing dechiper: %f seconds", time_spent_dechip);
-    
-    
-    //printf("Hasil decrypt : %08X%08X\n", v[0], v[1]);
-    //printf("Password anda: %s\n", output);
 }
 
 
@@ -101,26 +103,23 @@ void setup_wifi() {
 }
 
 void callback(char* topic, unsigned char* payload, unsigned int len) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-
-  // Print the payload as a string
-  for (int i = 0; i < len; i++) {
-    Serial.print((char)payload[i]);
-  }
   strncpy(msg, (char*)payload, len);
   //snprintf (msg, MSG_BUFFER_SIZE, "%s", decipher(32, msg, key));
+  clock_t start_decryption = clock();
   int temp = decipher(pengulangan, msg, key);
+  clock_t end_decryption = clock();
+  double decryption_time = (double)(end_decryption - start_decryption) / CLOCKS_PER_SEC;
   if (temp == 1)
     calon1 ++;
   else if (temp == 2)
     calon2 ++;
-  Serial.print(" == ");
-
+  Serial.print("Suara untuk Kak Gotawa : ");
   Serial.print(calon1);
+  printf("\n");
+  Serial.print("Suara untuk Pak Ogin : ");
   Serial.print(calon2);
   printf("\n");
+  printf("Decryption Time: %f seconds\n", decryption_time);printf("\n");
 
   if(calon1>calon2){
     digitalWrite(18, HIGH);
@@ -172,13 +171,9 @@ void setup() {
 }
 
 void loop() {
-  double time_spent_loop_dechiper = 0.0;
-  clock_t begin = clock();
+
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
-  clock_t end = clock(); //untuk mengetahui execution time program
-  time_spent_loop_dechip += (double)(end - begin) / CLOCKS_PER_SEC;
-  printf("Time spent for executing loop dechiper: %f seconds", time_spent_loop_dechip);
 }
